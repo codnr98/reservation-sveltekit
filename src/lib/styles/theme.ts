2a2a013a47780c13ced296b890cad80d7faf980f
codnr <wnd75913@gmail.com>
2023-10-18 10:49:26 +0900

    feat: theme 파일 생성

---
+++ src/lib/styles/theme.ts
@@ -0,0 +1,84 @@
+const palette = {
+	orange200: '#F1A68C',
+	orange400: '#FA8252',
+	orange500: '#ec541f',
+	orange700: '#E04E1A',
+
+	black50: '#d0cfcf',
+	black100: '#d0cfcf',
+	black200: '#a0a09f',
+	black300: '#717070',
+	black400: '#414140',
+	black500: '#121110',
+	black600: '#0e0e0d',
+	black700: '#0b0a0a',
+
+	background: {
+		primary: '#FFFFFF',
+		primary2: '#FBFBFB',
+		primary3: '#D5D3D1',
+		secondary: '#F5F5F4'
+	},
+
+	line: {
+		normal: '#D6D4D2'
+	},
+
+	secondary: {
+		red: '#ffc1bd',
+		yellow: '#ffe99b',
+		green: '#a7f9b4',
+		blue: '#aad6ff',
+		purple: '#eeb9ff'
+	}
+};
+
+const textStyle = {
+	headLine02: {
+		fontSize: '24px',
+		lineHeight: '32px'
+	},
+	title011: {
+		fontSize: '22px',
+		lineHeight: '26px'
+	},
+	title01: {
+		fontSize: '20px',
+		lineHeight: '26px'
+	},
+	title02: {
+		fontSize: '17px',
+		lineHeight: '24px'
+	},
+	body01: {
+		fontSize: '16px',
+		lineHeight: '26px'
+	},
+	body02: {
+		fontSize: '15px',
+		lineHeight: '24px'
+	},
+	label02: {
+		fontSize: '15px',
+		lineHeight: '24px'
+	},
+	label03: {
+		fontSize: '14px',
+		lineHeight: '20px'
+	},
+	caption01: {
+		fontSize: '12px',
+		lineHeight: '16px'
+	},
+	caption02: {
+		fontSize: '11px',
+		lineHeight: '14px'
+	}
+};
+
+const theme = {
+	palette,
+	textStyle
+};
+
+export default theme;
