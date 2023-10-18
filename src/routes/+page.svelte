3e3a4e72079ad3220f5dcf76b0f91a7ac5c29c3d
codnr <wnd75913@gmail.com>
2023-10-18 12:09:57 +0900

    fix: theme 파일 확장자 변경 및 내용 수정

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,21 @@
+<script>
+	import theme from '$lib/styles/theme';
+</script>
+
 <header>
-	<button />
+	<button class="reservation-button">reservation</button>
+	<h1>Reservation</h1>
+	<button>x</button>
 </header>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		padding: 5px 10px;
+	}
+
+	.reservation-button {
+	}
+</style>
