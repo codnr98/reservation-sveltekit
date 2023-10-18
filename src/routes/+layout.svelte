da1ab229197a97dbe5f35e61d1bb02ed09c075a8
codnr <wnd75913@gmail.com>
2023-10-18 12:17:03 +0900

    feat: theme 파일 layout에 import

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
-  import '../lib/styles/reset.css'
+	import '../lib/styles/reset.css';
+	import '../lib/styles/theme.css';
 </script>
 
-<slot></slot>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import theme from '$lib/styles/theme';
 </script>
 
 <header>
@@ -7,15 +6,12 @@
 	<h1>Reservation</h1>
 	<button>x</button>
 </header>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <style>
 	header {
 		display: flex;
 		justify-content: space-between;
 		padding: 5px 10px;
-	}
-
-	.reservation-button {
+		background-color: var(--background-primary);
 	}
 </style>
