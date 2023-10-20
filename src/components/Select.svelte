ec32c78a61e7445c674f91580c745a678cdba25a
codnr <wnd75913@gmail.com>
2023-10-20 13:17:14 +0900

    feat: select 컴포넌트 마크업

--- src/components/Select.svelte
+++ src/components/Select.svelte
@@ -1,38 +1,45 @@
 <script>
 	import DorpDownIcon from '../assets/arrow_drop_down.svelte';
+
+	let isDrop = false;
+
+	const handlerClickSelect = () => {
+		isDrop = !isDrop;
+	};
 </script>
 
 <div class="select-wrapper">
-	<div class="select">
+	<div class="select" role="button" on:click={handlerClickSelect}>
 		<ul class="select-item-container">
 			<li>hiufasdjf</li>
 			<li>hiufasdjf</li>
 		</ul>
-		<!-- <option>Select Table</option>
-		<option>option1</option>
-		<option>option2</option>
-		<option>option3</option> -->
-		<div class="icon-wrapper"><DorpDownIcon /></div>
+		<DorpDownIcon />
 	</div>
+	<ul class="option-list {isDrop && 'disable'}">
+		<li>1</li>
+		<li>1</li>
+		<li>1</li>
+		<li>1</li>
+	</ul>
 </div>
 
 <style>
 	.select-wrapper {
-		border: solid 1px var(--line-normal);
-		border-radius: 12px;
 		width: 55%;
+		position: relative;
 	}
 
 	.select {
 		/* padding: 22px 22px; */
+		border: solid 1px var(--line-normal);
+		border-radius: 12px;
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
 		padding: 0 16px;
 		color: var(--line-normal);
-		width: 100%;
 		height: 100%;
-		background-color: rgba(255, 255, 255, 0);
 	}
 
 	.select-item-container {
@@ -40,6 +47,28 @@
 		gap: 10px;
 	}
 
-	.icon-wrapper {
+	.option-list {
+		z-index: 1;
+		background-color: var(--background-primary);
+		border-radius: 12px;
+		position: absolute;
+		width: 100%;
+		padding: 5px 16px;
+		margin-top: 10px;
+		border: solid 1px var(--line-normal);
+		color: var(--line-normal);
+	}
+
+	.option-list > li {
+		padding: 5px 0;
+		border-bottom: solid 1px var(--line-normal);
+	}
+
+	.option-list > li:last-child {
+		border-bottom: none;
+	}
+
+	.disable {
+		display: none;
 	}
 </style>
